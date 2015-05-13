7765e235575e566008363d06539808b2020f1b81
balaixianren <huixiang0922@gmail.com>
2015-05-13 13:57:16 +0800

    first commit

---
+++ tests/index-spec.js
@@ -0,0 +1,14 @@
+'use strict';
+
+var expect = require('expect.js');
+var Templatable = require('../index');
+
+/*globals describe, it*/
+
+describe('nd-templatable', function() {
+
+  it('normal usage', function() {
+
+  });
+
+});
